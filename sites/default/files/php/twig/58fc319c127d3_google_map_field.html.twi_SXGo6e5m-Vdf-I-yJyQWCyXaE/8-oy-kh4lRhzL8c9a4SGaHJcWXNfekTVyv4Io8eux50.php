a637b44ee78b33fb24d2eaf8b3468d42952b72d6
alemn03 <you@example.com>
2017-05-07 22:34:16 -0600

    Lab

---
+++ sites/default/files/php/twig/58fc319c127d3_google_map_field.html.twi_SXGo6e5m-Vdf-I-yJyQWCyXaE/8-oy-kh4lRhzL8c9a4SGaHJcWXNfekTVyv4Io8eux50.php
@@ -0,0 +1,102 @@
+<?php
+
+/* modules/google_map_field/templates/google_map_field.html.twig */
+class __TwigTemplate_a01c54fedcfed3e51735b1023577096fd9440fca18024e470e9a2f37a61b5484 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 2);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('if'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "<section class=\"google-map-field\">
+  ";
+        // line 2
+        if (($context["name"] ?? null)) {
+            // line 3
+            echo "  <h2>";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["name"] ?? null), "html", null, true));
+            echo "</h2>
+  ";
+        }
+        // line 5
+        echo "  <div class=\"map-container\" style=\"width:";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["width"] ?? null), "html", null, true));
+        echo ";height:";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["height"] ?? null), "html", null, true));
+        echo ";\" data-controls-show=\"";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["show_controls"] ?? null), "html", null, true));
+        echo "\" data-marker-show=\"";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["show_marker"] ?? null), "html", null, true));
+        echo "\" data-type=\"";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["type"] ?? null), "html", null, true));
+        echo "\" data-lat=\"";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["lat"] ?? null), "html", null, true));
+        echo "\" data-lon=\"";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["lon"] ?? null), "html", null, true));
+        echo "\" data-zoom=\"";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["zoom"] ?? null), "html", null, true));
+        echo "\"></div>
+</section>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/google_map_field/templates/google_map_field.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  54 => 5,  48 => 3,  46 => 2,  43 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "modules/google_map_field/templates/google_map_field.html.twig", "C:\\xampp\\htdocs\\drupal\\modules\\google_map_field\\templates\\google_map_field.html.twig");
+    }
+}
